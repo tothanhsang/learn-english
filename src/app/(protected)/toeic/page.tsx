8ca4b03b1309efd378cd877a94c64fe555f6c3cc
sangto-groove <sang.to@groovetechnology.com>
2026-02-05 00:02:45 +0700

    feat: Add TOEIC vocabulary lessons 13, 14, and 15, and update the TOEIC page.

--- src/app/(protected)/toeic/page.tsx
+++ src/app/(protected)/toeic/page.tsx
@@ -5,8 +5,11 @@ import { ToeicVocabContent } from "@/components/toeic/toeic-vocab-content"
 import { ToeicLessonSelector } from "@/components/toeic/toeic-lesson-selector"
 import lesson11 from "../../../../vocabularys-600-toeic/lessons-11.json"
 import lesson12 from "../../../../vocabularys-600-toeic/lessons-12.json"
+import lesson13 from "../../../../vocabularys-600-toeic/lessons-13.json"
+import lesson14 from "../../../../vocabularys-600-toeic/lessons-14.json"
+import lesson15 from "../../../../vocabularys-600-toeic/lessons-15.json"
 
-const lessons = [lesson11, lesson12]
+const lessons = [lesson11, lesson12, lesson13, lesson14, lesson15]
 
 export default function ToeicPage() {
   const [currentLessonIndex, setCurrentLessonIndex] = useState(0)
